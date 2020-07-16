262e33c29e0e220b0779ee845e155da44e9fa42e
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-16 14:45:07 +0200

    Add solved IntroduceYourshelf task

---
+++ week-01/day-04/src/IntroduceYourshelf.java
@@ -0,0 +1,11 @@
+public class IntroduceYourshelf {
+    public static void main(String[] args) {
+        String name = "Náday Zsolt";
+        int age = 31;
+        float height = 1.87f;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
