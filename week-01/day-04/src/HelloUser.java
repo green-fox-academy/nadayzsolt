adf52d4109f19eba45e5adcc416e7d71817f0fcc
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-16 17:24:35 +0200

    Add solved HelloUser task

---
+++ week-01/day-04/src/HelloUser.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please write here your name: ");
+        String userInput1 = scanner.nextLine();
+
+        System.out.println("Hello, " + userInput1 + "!");
+    }
+}
