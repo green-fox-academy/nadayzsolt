23d9dead41efb7ccb40e9bb723e8d1486fdb037c
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-16 15:08:12 +0200

    Add solved TwoNumbers task

---
+++ week-01/day-04/src/TwoNumbers.java
@@ -0,0 +1,19 @@
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 22;
+
+        out.println( a+b);
+        out.println(b-a);
+        out.println(b * a);
+        out.println((double)b / a);
+        out.println(b/a);
+        out.println(b%a);
+
+
+
+    }
+}
