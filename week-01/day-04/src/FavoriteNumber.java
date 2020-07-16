11a217550b60814d1c760096b2f64f066863168a
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-16 16:02:40 +0200

    Add solved FavoriteNumber task

---
+++ week-01/day-04/src/FavoriteNumber.java
@@ -0,0 +1,6 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int myFavoriteNr = 3419;
+        System.out.println("My favorite number is: " + myFavoriteNr);
+    }
+}
