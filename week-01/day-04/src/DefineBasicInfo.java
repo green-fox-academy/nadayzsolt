39c2f150fea5f759ae432601a09e8b2eaaa60b8a
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-16 16:25:27 +0200

    Add solved DefineBasicInfo task

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Zsolt Náday";
+        int age = 33;
+        double height = 1.83;
+        boolean married = false;
+
+        System.out.println("My name is " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I am " + height + "m tall.");
+        System.out.println("Any statement, which means, that I am married, is actually " + married + ".");
+    }
+}
