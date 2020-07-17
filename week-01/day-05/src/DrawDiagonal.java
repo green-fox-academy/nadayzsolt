15cd27e877593c8b6f87106882dc1eb87b6762bf
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-17 16:29:37 +0200

    Add DrawDiagonal task

--- week-01/day-05/src/DrawDiagonal.java
+++ week-01/day-05/src/DrawDiagonal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+
 public class DrawDiagonal {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    String userInput = "";
+    System.out.println("Add your number: " + userInput);
+    userInput = scanner.nextLine();
+    int inputInt = Integer.parseInt(userInput);
+
+    for (int i = 1; i < inputInt + 1; i++) {
+      if (i % 5 == 0 || i % 5 == 1) {
+        System.out.println("%%%%%");
+      } else if (i % 5 == 2) {
+        System.out.println("%%  %");
+      } else if (i % 5 == 3) {
+        System.out.println("% % %");
+      } else if (i % 5 == 4) System.out.println("%  %%");
+    }
   }
 }
