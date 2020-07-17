3b7e9cf6af85cbb7501435df15dbb1d7b1a10f31
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-17 14:31:30 +0200

    Add solved MultiplicationTable task

---
+++ week-01/day-05/src/MultiplicationTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    String userInput = "";
+    System.out.println("Add the number to be multiplied: " + userInput);
+    userInput = scanner.nextLine();
+
+
+    int inputInt = Integer.parseInt(userInput);
+    for (int i = 1; i < 11; i++) {
+  //    int multiplicationSum = i * inputInt;
+      System.out.println(i + " * " + inputInt + " = " + i * inputInt);
+    }
+
+  }
+}
