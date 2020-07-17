9d8ee3b250277694e82eb2c549e640c5530d86c9
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-17 16:02:46 +0200

    Actualized GuessTheNumber task

--- week-01/day-05/src/DrawTriangle.java
+++ week-01/day-05/src/DrawTriangle.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
 
-// Write a program that stores a number, and the user has to figure it out.
-// The user can input guesses, after each guess the program would tell one
-// of the following:
-
 public class DrawTriangle {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
--- week-01/day-05/src/GuessTheNumber.java
+++ week-01/day-05/src/GuessTheNumber.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+// Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+
 public class GuessTheNumber {
   public static void main(String[] args) {
     int myInteger = 5787;
