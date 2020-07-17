0aec28d762c936068538d1cbd92f472461100c35
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-17 14:53:45 +0200

    Add solved CountFromTo task

---
+++ week-01/day-05/src/CountFromTo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String userInput1 = "";
+    System.out.print("Write here the first number: " + userInput1);
+    userInput1 = scanner.nextLine();
+
+    scanner = new Scanner(System.in);
+    String userInput2 = "";
+    System.out.print("Write here the second number: " + userInput2);
+    userInput2 = scanner.nextLine();
+
+
+    int myInteger1 = Integer.parseInt(userInput1);
+    int myInteger2 = Integer.parseInt(userInput2);
+
+    for (int i = myInteger1; i < myInteger2; i++) {
+      if (myInteger2 > myInteger1) {
+        System.out.println(i);
+      }
+    }
+    if (myInteger2 <= myInteger1) {
+      System.out.println("The second number should be bigger");
+    }
+  }
+}
+
--- week-01/day-05/src/MultiplicationTable.java
+++ week-01/day-05/src/MultiplicationTable.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
     String userInput = "";
     System.out.println("Add the number to be multiplied: " + userInput);
     userInput = scanner.nextLine();
@@ -10,7 +10,6 @@ public class MultiplicationTable {
 
     int inputInt = Integer.parseInt(userInput);
     for (int i = 1; i < 11; i++) {
-  //    int multiplicationSum = i * inputInt;
       System.out.println(i + " * " + inputInt + " = " + i * inputInt);
     }
 
