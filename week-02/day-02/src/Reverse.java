d2363a22cb24f804d276c76a5e610efccbe7a3bd
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-21 17:55:47 +0200

    Actualization

--- week-02/day-02/src/Reverse.java
+++ week-02/day-02/src/Reverse.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Reverse {
   public static void main(String[] args) {
     String toBeReversed =
@@ -6,14 +8,18 @@ public class Reverse {
     // Use it on this reversed string to check it!
     // Try to solve this using charAt() first, and optionally anything else after.
 
-    int i = toBeReversed.length();                   //determining the length of the string
-    while (i > 0) {{
-      toBeReversed.charAt(i - 1);                 //printing the character at index i-1
-      i--;
-      //decreasing the length of the string
-    } toBeReversed = toBeReversed;};
-
-      System.out.println(toBeReversed);
+//    int i = toBeReversed.length();                   //determining the length of the string
+//    while (i > 0) {{
+//      toBeReversed.charAt(i - 1);                 //printing the character at index i-1
+//      i--;
+//      //decreasing the length of the string
+//    } toBeReversed = toBeReversed;};
+String b;
+    for (int i = toBeReversed.length(); i >= 0; i--) {
+      ArrayList
+          b = toBeReversed.charAt() ;
+    }
+      System.out.println(b);
 
   }
 }
