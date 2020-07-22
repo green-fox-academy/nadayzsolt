f3028d361b768910142d91027e6b6a947790c2cd
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-22 17:55:12 +0200

    daily finish

---
+++ week-02/day-03/src/CountLines.java
@@ -0,0 +1,11 @@
+public class CountLines {
+  public static void main(String[] args) {
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+    
+
+
+  }
+}
