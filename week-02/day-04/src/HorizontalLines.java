f388dfcfbb066c3f6c72b2ee628f9ed91f9167d2
nadayzsolt <nadayzsolt@gmail.com>
2020-07-23 14:08:36 +0200

    Add solved HorizontalLines task

---
+++ week-02/day-04/src/HorizontalLines.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that draws a single line and takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a 50 long horizontal line from that point.
+        // Draw at least 3 lines with that function using a loop.
+        int x1 = 234;
+        int y1 = 87;
+        theVeryFunction(graphics, x1, y1);
+    }
+
+    public static void theVeryFunction(Graphics graphics, int x1, int y1) {
+        graphics.setColor(Color.BLACK);
+        graphics.drawLine(x1, y1, (x1 + 50), y1);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
