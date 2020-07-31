d3c505bfbf7202efa1d6b6b9e7554a26e5cc0bcd
Zsolt Náday <nadayzsolt@gmail.com>
2020-07-31 08:17:45 +0200

    Add Anagram task (without the test for now)

--- week-03/day-04/tests/anagram/Anagram.java
+++ week-03/day-04/tests/anagram/Anagram.java
@@ -1,24 +1,47 @@
 package anagram;
-import java.util.Array.*;
-
+//
+//
 //    Write a function, that takes two strings and returns a boolean value,
 //    based on if the two strings are Anagramms or not.
 //     Create a test for that.
 
+//
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Anagram {
-  static String aStr = "bela";
-  static String bStr = "abel";
   public static void main(String[] args) {
-  char[] aArray = new char[];// initializer?
-  char[] bArray = new char[];// initializer?
-    aArray = aStr.toCharArray();
-    bArray = bStr.toCharArray();
-   boolean areTheyAnagram = aArray.equals(bArray);// nem müxik
-    System.out.println(areTheyAnagram);
+    String alphaStr = "Bela";
+    String betaStr = "Beca";
+    anagramCompare(alphaStr, betaStr);
   }
 
+  public static void anagramCompare(String aStr, String bStr) {
+    String aLowerStr = aStr.toLowerCase();
+    String bLowerStr = bStr.toLowerCase();
+    String aSplitStr[] = aStr.split("");
+    String bSplitStr[] = bStr.split("");
+    List<String> aList = new ArrayList<String>();
+    List<String> bList = new ArrayList<String>();
+    aList = Arrays.asList(aSplitStr);
+    bList = Arrays.asList(bSplitStr);
+    boolean aContainsB = aList.containsAll(bList);
+    boolean bContainsA = bList.containsAll(aList);
 
+    if (aStr.contentEquals(bStr)) {
+      System.out
+          .println(aStr + " & " + bStr + " are just the same (it's more then to be anagrams.)");
+    } else if (aContainsB == true && bContainsA == true) {
+      System.out.println(aStr + " & " + bStr + " are perfect anagrams of each other.");
+    } else if (aStr.length() != bStr.length() && aContainsB == true && bContainsA == true) {
+      System.out.println(
+          aStr + " & " + bStr + " are technically not anagrams, however contain the same letters.");
+    } else if (aStr.length() == bStr.length()) {
+      System.out.println(aStr + " & " + bStr + " are not anagrams of each other.");
+    } else {
+      System.out.println(aStr + " & " + bStr + " are not anagrams, not even the same length");
+    }
+  }
 }
