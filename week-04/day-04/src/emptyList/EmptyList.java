0e5716c30cdc8811b3c0d88f1fe241e8500f6f76
nadayzsolt <nadayzsolt@gmail.com>
2020-08-06 07:51:43 -0400

    Add EmptyList task solved

---
+++ week-04/day-04/src/emptyList/EmptyList.java
@@ -0,0 +1,24 @@
+package emptyList;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class EmptyList {
+  public static void main(String[] args) {
+    Path filePath = Paths.get("/home/zsolt/IdeaProjects/nadayzsolt/week-04/day-04/src/emptyList/empty.txt");
+    List<String> lines = null;
+    try {
+      lines = Files.readAllLines(filePath);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("brühbrüh");
+    }
+    if (args[0].equals("-l") && lines != null) {
+      System.out.println("No todos for today! :)");
+    }
+  }
+}
+
