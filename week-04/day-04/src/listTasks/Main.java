a02e07bdf01b69b9fdedb75538dabc761d5e5546
nadayzsolt <nadayzsolt@gmail.com>
2020-08-06 06:12:46 -0400

    Add task in an early stage of progress

---
+++ week-04/day-04/src/listTasks/Main.java
@@ -0,0 +1,28 @@
+package listTasks;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    if (args[0]equals("-l")) {
+      Path filePath = Paths.get("/home/zsolt/IdeaProjects/nadayzsolt/week-04/day-04/src/listTasks/file.txt");
+      List<String> lines = null;
+      try {
+        lines = Files.readAllLines(filePath);
+        int index = 0;
+        for (String bela : lines) {
+          System.out.print(++index + ". ");
+          System.out.println(bela);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("brühbrüh");
+      }
+    }
+  }
+}
---
+++ week-04/day-04/src/printUsage/Main.java
@@ -0,0 +1,9 @@
+package printUsage;
+
+public class Main {
+  public static void main(String[] args) {
+    for (String bela: args) {
+      System.out.println(bela);
+    }
+  }
+}
