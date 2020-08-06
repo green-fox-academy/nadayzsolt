4c6936a30608cf0e896aba26aa09e3fd31f227d7
nadayzsolt <nadayzsolt@gmail.com>
2020-08-06 10:36:08 -0400

    Final solution to AddNewTask with the absolute path readable for CLI as well

--- week-04/day-04/src/addNewTask/AddNewTask.java
+++ week-04/day-04/src/addNewTask/AddNewTask.java
@@ -1,26 +1,21 @@
 package addNewTask;
 
-import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AddNewTask {
-  static Path filePath = Paths.get("week-04/day-04/src/addNewTask/textAppended.txt");
+  static Path filePath = Paths.get("/home/zsolt/IdeaProjects/nadayzsolt/week-04/day-04/src/addNewTask/textAppended.txt");
 
   public static void main(String[] args) throws IOException {
-    for (int i = 1; i < args.length; i++) {
-      String theArg = "";
-      if (args[0].equals("-a")) {
-        theArg = args[i] + "\n";
-      }
-      System.out.println(theArg);
+    if (args[0].equals("-a")) {
+      System.out.println(args[1]);
+      String theArg = "\n" + args[1];
       Files.write(filePath, theArg.getBytes(), StandardOpenOption.APPEND);
+    } else {
+      System.out.println("bad operation");
     }
   }
 }
