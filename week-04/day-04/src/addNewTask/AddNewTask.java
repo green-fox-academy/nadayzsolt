e7142205c840a1f73201e4e36e951df28ba01306
Zsolt Náday <nadayzsolt@gmail.com>
2020-08-06 21:31:23 +0200

    Add new method to AddNewTask-task

--- week-04/day-04/src/addNewTask/AddNewTask.java
+++ week-04/day-04/src/addNewTask/AddNewTask.java
@@ -1,10 +1,14 @@
 package addNewTask;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddNewTask {
   static Path filePath = Paths
@@ -13,12 +17,38 @@ public class AddNewTask {
   public static void main(String[] args) throws IOException {
     if (args[0].equals("-a") && args.length == 1) {
       System.out.println("Unable to add: no task provided");
-    } else if (args[0].equals("-a") && args.length != 1){
+    } else if (args[0].equals("-a") && args.length != 1) {
       System.out.println(args[1]);
       String theArg = "\n" + args[1];
       Files.write(filePath, theArg.getBytes(), StandardOpenOption.APPEND);
-    } else{
-      System.out.println("bad operation");
+    } else if (args[0].equals("-r") && args[1].equals("2")) {
+      manageTextAppendFile();
+    } else {
+      System.out.println("Watch out, pal! It's a pity, You have access to this mighty program...");
+    }
+  }
+
+  public static void manageTextAppendFile() throws IOException {
+    Scanner scanner = new Scanner(filePath);
+    ArrayList<String> coll = new ArrayList<String>();
+    scanner.nextLine();
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      coll.add(line);
+    }
+
+    scanner.close();
+
+    FileWriter writer = new FileWriter("week-04\\day-04\\src\\addNewTask\\textAppended.txt");
+    String nullStr = "";
+    for (int i = 0; i < coll.size(); i++) {
+//      if (i == 1) {
+//        writer.write(nullStr);
+//      } else {
+        writer.write("week-04\\day-04\\src\\addNewTask\\textAppended.txt");
+//      }
+
+      writer.close();
     }
   }
 }
