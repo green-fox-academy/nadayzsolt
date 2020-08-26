5bf7fcaada8b5add745dfe474d0c4a0deb48fde9
Zsolt Náday <nadayzsolt@gmail.com>
2020-08-26 17:48:31 +0200

    Add cleaned Say Hello to all the world task

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -10,20 +10,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
-  AtomicLong counterStrike = new AtomicLong();
-  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
-      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  AtomicLong counter = new AtomicLong();
+  String[] hellos =
+      {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag",
+          "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló",
+          "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry",
+          "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae",
+          "Sholem Aleychem", "Sawubona"};
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model, @RequestParam (required = false) String name) {
+  public String greeting(Model model, @RequestParam(required = false) String name) {
     Random random = new Random();
-    int counter = random.nextInt(50);
-    String randomLanguageHello = hellos[counter];
+    int randomNum = random.nextInt(50);
+    String randomLanguageHello = hellos[randomNum];
     model.addAttribute("name", name);
     model.addAttribute("randLangHello", randomLanguageHello);
-    model.addAttribute("counter", counterStrike.incrementAndGet());
+    model.addAttribute("counter", counter.incrementAndGet());
     return "greeting";
   }
 }
