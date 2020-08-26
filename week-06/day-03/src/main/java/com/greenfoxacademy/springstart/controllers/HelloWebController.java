7249c38d4159805dc43b64efca1d409ed6601870
Zsolt Náday <nadayzsolt@gmail.com>
2020-08-26 17:35:05 +0200

    Add solved Say Hello to all the world task

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.controllers;
 
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
-  AtomicLong counter = new AtomicLong();
+  AtomicLong counterStrike = new AtomicLong();
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam (required = false) String name) {
+    Random random = new Random();
+    int counter = random.nextInt(50);
+    String randomLanguageHello = hellos[counter];
     model.addAttribute("name", name);
-    model.addAttribute("counter", counter.incrementAndGet());
+    model.addAttribute("randLangHello", randomLanguageHello);
+    model.addAttribute("counter", counterStrike.incrementAndGet());
     return "greeting";
   }
 }
