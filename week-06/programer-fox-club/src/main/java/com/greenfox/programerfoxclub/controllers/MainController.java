3eb0cbda25b954cbef5a3850eface026d45f64e7
nadayzsolt <nadayzsolt@gmail.com>
2020-09-03 17:41:23 +0200

    Add probably working listing function to LoginPostPetName

--- week-06/programer-fox-club/src/main/java/com/greenfox/programerfoxclub/controllers/MainController.java
+++ week-06/programer-fox-club/src/main/java/com/greenfox/programerfoxclub/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.greenfox.programerfoxclub.models.Fox;
 import com.greenfox.programerfoxclub.services.FoxService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
   FoxService foxService;
 
-
+  @Autowired
   public MainController(FoxService foxService) {
     this.foxService = foxService;
   }
@@ -31,17 +32,34 @@ public class MainController {
     for (Fox fox : foxService.foxes) {
       foxNames.add(fox.getName());
     }
+//    System.out.println(foxNames);
     if (!foxNames.contains(name)) {
-      Fox fox = new Fox (name);
-      foxService.foxes.add(fox);
+      foxService.foxes.add(new Fox(name));
     }
-    return"redirect:/?name="+name;
+    return "redirect:/?name=" + name;
   }
 
 
   @GetMapping("/")
   public String loginGetPetName(Model model, @RequestParam(required = false) String name) {
     model.addAttribute("name", name);
+    Fox existFox =
+        foxService.foxes.stream().filter(fox -> fox.getName().equals(name)).findFirst()
+            .orElse(null);
+    model.addAttribute("food", existFox.getFood());
+    model.addAttribute("drink", existFox.getDrink());
+    int tricksAmount = existFox.getTricks().size();
+    model.addAttribute("trickAmount", tricksAmount);
+    if (tricksAmount > 1) {
+      model.addAttribute("trickcanbeplural", "s");
+    } else {
+      model.addAttribute("trickcanbeplural", "");
+    }
+    if (tricksAmount > 0) {
+      model.addAttribute("tricks", existFox.getTricks());
+    } else {
+      model.addAttribute("tricks", "You know no tricks, yet. Go and learn some.");
+    }
     return "index";
   }
 }
