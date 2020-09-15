e0766a2a029f147d12e6682dc00f0037a086424a
nadayzsolt <nadayzsolt@gmail.com>
2020-09-15 13:29:38 +0200

    Add Frontend task

---
+++ week-08/backendapi/src/main/java/com/example/backendapi/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.example.backendapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping ("/")
+  public String renderMain (){
+    return "index";
+  }
+}
