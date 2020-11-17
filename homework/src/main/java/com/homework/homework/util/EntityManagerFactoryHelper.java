fbdf14ea172997fedd63db746c1fdbdae9ed6a8a
nadayzsolt <nadayzsolt@gmail.com>
2020-11-17 22:40:58 +0100

    Add EntityManagerFactoryHelper.class simplified, over; this the end! Really finished. This time not joking...

--- homework/src/main/java/com/homework/homework/util/EntityManagerFactoryHelper.java
+++ homework/src/main/java/com/homework/homework/util/EntityManagerFactoryHelper.java
@@ -4,11 +4,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EntityManagerFactoryHelper {
-
-  public static EntityManagerFactory factory;
-
-  private static final String persistenceUnitName = null;
-
-  EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+  EntityManagerFactory emf = Persistence.createEntityManagerFactory(null);
 }
 
